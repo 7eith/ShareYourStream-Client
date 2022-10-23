d5f338431c4fe75f1e212ecd223d5866f4a83ef2
Alexis 'seith' M <amontelimart@gmail.com>
2022-10-23 05:32:53 +0200

    :sparkles: add: landingPage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,38 @@
 const HomePage = () => {
 
+    const handleMouseMovement = (e: any) => {
+        const el = document.getElementsByClassName("homePage")[0];
+        const d = el!.getBoundingClientRect();
+        let x = e.clientX - (d.left + Math.floor(d.width / 2));
+        let y = e.clientY - (d.top + Math.floor(d.height / 2));
+
+        // Invert values
+        x = x - x * 2;
+        y = y - y * 2;
+        document.documentElement.style.setProperty("--scale", "1.6");
+        document.documentElement.style.setProperty("--x", x / 2 + "px");
+
+        document.documentElement.style.setProperty("--y", y / 2 + "px");
+    };
+
+    const handleMouseLeave = () => {
+        document.documentElement.style.setProperty("--scale", "1");
+        document.documentElement.style.setProperty("--x", "0");
+        document.documentElement.style.setProperty("--y", "0");
+    };
+
     return (
-       <>
-        HELLOO
-       </>
+        <div 
+            className="homePage"
+            onMouseMove={handleMouseMovement}
+            onClick={handleMouseLeave} 
+        >
+            <img className="backgroundImg" />
+            <div className="title">
+                <h1>Share Your Stream</h1>
+                <p>An enhancer for Spotify & YouTube</p>
+            </div>
+        </div>
     )
 }
 
