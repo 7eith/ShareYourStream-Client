7c4e828e36dc3afa0ea9ca81f8a3e3dcdf426536
Alexis 'seith' M <amontelimart@gmail.com>
2022-11-01 01:15:26 +0100

    :hammer: fix: fixed i18n for Password Forgot

--- src/translations/en/path.ts
+++ src/translations/en/path.ts
@@ -2,14 +2,21 @@ const PathTranslations = {
     "/": "Home",
     "/app": "Dashboard",
     "/app/profile": "Profile",
+
     "/auth/signUp": "Sign Up",
     "/auth/signUp.text": "Already Member ? ",
     "/auth/signUp.label": "Sign In",
     "/auth/signUp.link": "/auth/signIn",
+    
     "/auth/signIn": "Sign In",
     "/auth/signIn.text": "Not member ? ",
     "/auth/signIn.label": "Sign Up",
     "/auth/signIn.link": "/auth/signUp",
+
+    "/auth/passwordForgot": "Password Forgot",
+    "/auth/passwordForgot.text": "Not member ? ",
+    "/auth/passwordForgot.label": "Sign In",
+    "/auth/passwordForgot.link": "/auth/signIn",
 };
   
 export default PathTranslations;
